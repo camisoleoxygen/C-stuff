cd47482da64940962cdfc2aea8677bace8555e42
Julian <synnjulian@gmail.com>
2018-02-12 13:36:45 -0500

    Add files via upload

---
+++ oop244/ws4/in_lab_4/Passenger.cpp
@@ -0,0 +1,24 @@
+// TODO: add file header comments here
+
+// TODO: add your headers here
+#include <iostream>
+#include "Passenger.h"
+
+// TODO: continue your namespace here
+using namespace std;
+using namespace sict;
+
+// TODO: implement the default constructor here
+Passenger::Passenger() {
+
+}
+
+// TODO: implement the constructor with 2 parameters here
+
+
+// TODO: implement isEmpty query here
+	bool isEmpty() const:
+
+// TODO: implement display query here
+	void display() const :
+
---
+++ oop244/ws4/in_lab_4/Passenger.h
@@ -0,0 +1,23 @@
+// TODO: add file header comments here
+
+
+// TODO: add header file guard here
+#ifndef SICT_PASSENGER_H
+#define SICT_PASSENGER_H
+
+// TODO: declare your namespace here
+namespace sict{
+
+// TODO: define the Passenger class here
+	class Passenger {
+		char passengerName[32] = '\0';
+		char destination[32] = '\0';
+
+	};
+
+
+};
+
+
+
+#endif
