f9d76a1db6bf9cda0c65032a62492415d3df4f9e
Julian <synnjulian@gmail.com>
2018-02-13 14:16:17 -0500

    Update Passenger.h

--- oop244/ws4/in_lab_4/Passenger.h
+++ oop244/ws4/in_lab_4/Passenger.h
@@ -1,22 +1,36 @@
 // TODO: add file header comments here
+// Workshop 4 - Constructors - Test Main
+// Passenger.h
+// Hoh-il (Julian) Synn
+// Feb 12, 2018
 
 
 // TODO: add header file guard here
 #ifndef SICT_PASSENGER_H
 #define SICT_PASSENGER_H
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <string.h>
 
 // TODO: declare your namespace here
-namespace sict{
+namespace sict {
 
 // TODO: define the Passenger class here
 	class Passenger {
-		char passengerName[32] = '\0';
-		char destination[32] = '\0';
+		char passengerName[32];
+		char destination[32];
+		
+	public:
+		Passenger();
+
+		Passenger(const char* passenger_char, const char* destination_char); 
+
+		bool isEmpty() const;
+		void display() const;
 
 	};
 
 
-};
+}
 
 
 
