287d57611cbe7a18843678d321be85eae0c2668e
Julian <synnjulian@gmail.com>
2018-03-08 10:09:28 -0500

    Add files via upload

---
+++ oop244/ws5B/at_home_5B/Fraction.h
@@ -0,0 +1,60 @@
+// Workshop 5B: operator overloading
+// File: Fraction.h
+// Name: Julian (Hoh-il) Synn
+// Date: Mar 7, 2018
+/////////////////////////////////////////////
+
+
+// TODO: header file guard
+#ifndef SICT_FRACTION_H
+#define SICT_FRACTION_H
+
+// TODO: create namespace
+namespace sict {
+
+	// TODO: define the Fraction class
+	class Fraction {
+
+
+		// TODO: declare instance variables 
+		int numerator;
+		int denominator;
+
+		// TODO: declare private member functions
+		int max() const;
+		int min() const;
+		void reduce();
+		int gcd() const;
+
+	public:
+		// TODO: declare public member functions
+		bool isEmpty() const;
+		void display() const;
+		Fraction(); //default constructor
+		Fraction(int, int); //constructor with 2 parameters
+
+		// TODO: declare the + operator overload
+		Fraction operator+(const Fraction& rhs) const;
+
+		//TODO: declare additional operator overloads
+		Fraction operator*(const Fraction&) const;
+
+
+
+		//bool operator==(const Fraction&) const;
+		//bool operator!=(const Fraction&) const;
+
+
+
+		//TODO for Lab 5B: friend operators
+		friend bool operator==(const Fraction&, const Fraction&);
+		friend bool operator!=(const Fraction&, const Fraction&);
+
+
+
+		//+= operator
+		Fraction& operator+=(const Fraction&);
+	};
+
+}
+#endif
