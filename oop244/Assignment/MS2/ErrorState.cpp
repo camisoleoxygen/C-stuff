6dfa116e47025e3a10b7940e7fc9871914783d01
Julian <synnjulian@gmail.com>
2018-03-19 09:54:43 -0400

    Add files via upload

---
+++ oop244/Assignment/MS2/ErrorState.cpp
@@ -0,0 +1,15 @@
+//ErrorState.cpp
+//Julian (Hoh-il) Synn
+//OOP244 - Milestone 2
+//Date: Mar 17, 2018
+
+#include "ErrorState.h"
+#include <iostream>
+using namespace std;
+
+namespace AMA {
+
+
+
+
+}
---
+++ oop244/Assignment/MS2/ErrorState.h
@@ -0,0 +1,11 @@
+//ErrorState.h
+//Julian (Hoh-il) Synn
+//OOP244 - Milestone 2
+//Date: Mar 17, 2018
+#include <iostream>
+
+namespace AMA {
+
+
+
+}
